d5007746a506303b7a382534d68dc0d8a85bf3d2
段体华 <duantihua@gmail.com>
2023-08-01 15:29:42 +0800

    update ajaxchosen load before search

--- src/main/resources/js/beangle-ui-ajaxchosen.js
+++ src/main/resources/js/beangle-ui-ajaxchosen.js
@@ -18,7 +18,8 @@
       if (!chosenOptions.no_results_text) {
         chosenOptions.no_results_text = "没有找到结果!";
       }
-      this.chosen(chosenOptions);
+
+      this.chosen(chosenOptions);//first init chosen
       var search_field;
       if (this.prop('multiple')) {
         search_field = this.next('.chosen-container').find(".search-field > input");
@@ -26,8 +27,75 @@
         search_field = this.next('.chosen-container').find(".chosen-search > input");
       }
 
+      /**fill in data to select*/
+      function fillin(data){
+        var items, nbItems, selected_values;
+        if (data == null) {
+          return;
+        }
+        selected_values = [];
+        select.find('option').each(function() {
+          if (!$(this).is(":selected")) {
+            return $(this).remove();
+          } else {
+            return selected_values.push($(this).val()  + "-" + $(this).text());
+          }
+        });
+        items = callback(data);
+        $.each(items, function(i, obj) {
+          if($.inArray(obj.value + "-" + obj.text, selected_values) === -1) {
+            return $("<option />").val(obj.value).html(obj.text).appendTo(select);
+          }
+        });
+        select.trigger("chosen:updated.chosen");
+        return items.length;
+      }
+      function triggerChosen(nbItems,search_value){
+        var chosen= select.data().chosen;
+        if (nbItems) {
+          //search_field value missing after update.chosen
+          search_field.val(search_value);
+          chosen.results_search();
+        }else{
+          if(typeof chosenOptions.as_combobox != "undefined" && chosenOptions.as_combobox){
+            select.find('option').each(function() {
+              if ($(this).val().startsWith('0:')) return $(this).remove();
+            });
+            $("<option selected='selected'/>").val('0:'+search_value).html(search_value).prependTo(select);
+            select.trigger("chosen:updated.chosen");
+          }else{
+            chosen.update_results_content("");
+            select.data().chosen.no_results(search_field.val());
+          }
+        }
+        return search_field.val(search_value);
+      }
+
+      function fetchData(options,triggleSearch,search_value){
+        if (options.data == null) {
+          options.data = {};
+        }
+        options.url = settings.url.replace("{term}",search_value);
+        if (options.postData != undefined) {
+          var extraData = options.postData();
+          $.each(extraData, function(key, value) {
+            options.data[key] = value;
+          });
+        }
+        options.success = function(data) {
+          var nItems=fillin(data)
+          if (settings.success != null) {
+            settings.success(data);
+          }
+          if(triggleSearch){
+            triggerChosen(nItems,search_value)
+          }
+        };
+        return $.ajax(options);
+      }
+
       search_field.bind('ajaxchosen', function() {
-        var field, success, raw_val, val;
+        var field, raw_val, val;
         raw_val = $(this).val();
         if(raw_val == chosenOptions.placeholder_text){
           raw_val="";
@@ -41,63 +109,10 @@
           return false;
         }
         field = $(this);
-        if (options.data == null) {
-          options.data = {};
-        }
-        options.url = settings.url.replace("{term}",val);
-        if (options.postData != undefined) {
-          var extraData = options.postData();
-          $.each(extraData, function(key, value) {
-            options.data[key] = value;
-          });
-        }
-        options.success = function(data) {
-          var items, nbItems, selected_values;
-          if (data == null) {
-            return;
-          }
-          selected_values = [];
-          select.find('option').each(
-              function() {
-                if (!$(this).is(":selected")) {
-                  return $(this).remove();
-                } else {
-                  return selected_values.push($(this).val()
-                      + "-" + $(this).text());
-                }
-              });
-          items = callback(data);
-          nbItems = items.length;
-          $.each(items, function(i, obj) {
-            if($.inArray(obj.value + "-" + obj.text, selected_values) === -1) {
-              return $("<option />").val(obj.value).html(obj.text).appendTo(select);
-            }
-          });
-          var chosen= select.data().chosen;
-          if (nbItems) {
-            select.trigger("chosen:updated.chosen");
-            //field value missing after update.chosen
-            field.val(raw_val);
-            chosen.results_search();
-          }else{
-            if(typeof chosenOptions.as_combobox != "undefined" && chosenOptions.as_combobox){
-              select.find('option').each(function() {
-                if ($(this).val().startsWith('0:')) return $(this).remove();
-              });
-              $("<option selected='selected'/>").val('0:'+raw_val).html(raw_val).prependTo(select);
-              select.trigger("chosen:updated.chosen");
-            }else{
-              chosen.update_results_content("");
-              select.data().chosen.no_results(field.val());
-            }
-          }
-          if (settings.success != null) {
-            settings.success(data);
-          }
-          return field.val(raw_val);
-        };
-        return $.ajax(options);
+        return fetchData(options,true,val);
       });
+
+      fetchData(options,false,"");
 
       var inComposition=false;
       search_field.on('compositionstart',function(){inComposition=true;})
